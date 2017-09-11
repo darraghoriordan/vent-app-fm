747788844a9e8d2c0ca8044e5960889bafab3f5e
Darragh ORiordan <darragh.oriordan@gmail.com>
2017-09-11 13:20:31 +1200

    just adding some encouragement

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 new Vue({
   el: '#editor',
   data: {
-    inputvent: 'start venting...'
+    inputvent: '',
+    placeholderMessage: "start venting..."
   },
   watch: {
-    
+
     inputvent: function (ventText) {
       this.ventamatic()
     }
   },
   methods: {
     ventamatic: _.debounce(function (e) {
-      this.inputvent = ''
-    }, 800)
+      this.inputvent = '';
+      this.placeholderMessage = this.selectNewPlaceholderMessage();
+    }, 3000),
+    selectNewPlaceholderMessage: function () {
+      const messages = ["start venting...", "keep going...", "I need to hear more...", "You'll feel better soon..."];
+      let selectedIndex = Math.floor((Math.random() * messages.length));
+      return messages[selectedIndex];
+    }
   }
 })
